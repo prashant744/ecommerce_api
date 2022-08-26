23fcd2aaa0be175438836bbce7c4327a90ffb85b
Prashant Dhote <prashantdhote400@gmail.com>
2022-08-26 23:55:45 +0530

    add pakages

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
 
 dotenv.config();
 
@@ -20,6 +22,8 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 
 app.listen(port, function (err) {
   if (err) {
