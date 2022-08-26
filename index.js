33b459a2e8f6b964e1f0db7cc7caf805b9c917cb
Prashant Dhote <prashantdhote400@gmail.com>
2022-08-26 21:45:23 +0530

    login and update path setup

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB connection Successfull"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+
+app.listen(port, function (err) {
+  if (err) {
+    console.log("Error in running the server", err);
+  }
+  console.log("Yup!My Server is running on Port", port);
+});
