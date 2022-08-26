72e04360f927cd188f4af3ea1650dacbff9afe36
Prashant Dhote <prashantdhote400@gmail.com>
2022-08-26 22:00:13 +0530

    update verifyTokenAndAdmin  function

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -24,7 +24,18 @@ const verifyTokenAndAuthorization = (req, res, next) => {
   });
 };
 
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not alowed to do that!");
+    }
+  });
+};
+
 module.exports = {
   verifyToken,
   verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
 };
