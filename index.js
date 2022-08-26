b4690419a15807f6d510a431bab70fca85ea452b
Prashant Dhote <prashantdhote400@gmail.com>
2022-08-26 23:24:18 +0530

    update products file

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ mongoose
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
 
 app.listen(port, function (err) {
   if (err) {
