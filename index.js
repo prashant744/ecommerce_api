d96ae57e4b0a62ed1c4a5a92c18bed6a34aa217f
Prashant Dhote <prashantdhote400@gmail.com>
2022-08-27 17:26:19 +0530

    update index file

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -18,12 +20,14 @@ mongoose
     console.log(err);
   });
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(port, function (err) {
   if (err) {
